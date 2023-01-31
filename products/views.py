25684b9af81524e216b8e6d3e4e120160e8fc4e1
Azam <abdurazakovazam01@gmail.com>
2023-01-31 16:35:33 +0500

    add apps products, dry

---
+++ products/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+
+def index(request):
+    context = {
+        'title': 'Store'
+    }
+    return render(request, 'products/index.html', context)
+
+
+def products(request):
+    context = {
+        'title': 'Store - Каталог',
+        'products': [
+            {
+                'image': '/static/img/products/Adidas-hoodie.png',
+                'name': 'Худи черного цвета с монограммами adidas Originals',
+                'price': '6090',
+                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'
+            }
+    ]
+    }
+    return render(request, 'products/products.html', context)
